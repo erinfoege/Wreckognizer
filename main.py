e7e7e24de53b1c972a797f7e308c0a0498c8ccf3
Erin Foege <160276635+erinfoege@users.noreply.github.com>
2025-04-17 17:32:54 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+# Set the path to the file you'd like to load
+file_path = ""
+
+# Load the latest version
+df = kagglehub.load_dataset(
+  KaggleDatasetAdapter.PANDAS,
+  "sobhanmoosavi/us-accidents",
+  file_path,
+  # Provide any additional arguments like 
+  # sql_query or pandas_kwargs. See the 
+  # documenation for more information:
+  # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
+)
