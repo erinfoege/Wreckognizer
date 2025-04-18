5aa6defde7d9c275fdc64de336fc029e52ae4f35
erinfoege <erinfoege15@gmail.com>
2025-04-18 18:12:21 -0400

    Data (zip)

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pandas as pd
 file_path = "data/US_Accidents_March23.csv";
 
 # Read first 100,000 rows
-df = pd.read_csv(file_path, usecols=['ID', 'Severity', 'Start_Lat', 'Start_Lng'],
-    nrows=100000)
+df = pd.read_csv(file_path, usecols=['ID', 'Severity', 'Start_Lat', 'Start_Lng','Zipcode','State', 'City'],
+    nrows= 100000)
 
 print(df.head())
 
+df.to_csv("data/US_Accidents_Sample_smaller.csv", index=False)
