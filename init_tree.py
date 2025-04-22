09f4b4ae6fc3306ffe50626bba5f051c6189a57e
Kylen Williams <kylenwilliams99@gmail.com>
2025-04-22 18:15:16 -0400

    adds zipped tree functionality and zipped tree pickle to make inital runs faster.

--- init_tree.py
+++ init_tree.py
@@ -3,22 +3,24 @@ import pickle
 import pandas as pd
 from kdTree import insert_rec
 from quadTree import insert_quad
+import zipfile
 
-PICKLE_FILE = "data/trees.pkl"
+PICKLE_FILE = "data/trees.pkl.zip"
 CSV_FILE = "data/US_Accidents_2022_and_up.zip"
 
-# os.remove("data/trees.pkl") # uncomment this to build the tree live before running it
+# os.remove("data/trees.pkl.zip") # uncomment this to build the tree live before running it
 
 def build_trees():
     # this builds the tree from a pickle of the tree, so that we don't have to remake it each time
     if os.path.exists(PICKLE_FILE):
-        with open(PICKLE_FILE, "rb") as f:
-            print("loading crash site trees from pickle...")
-            return pickle.load(f)
+        print("loading trees from zipped pickle file...")
+        with zipfile.ZipFile(PICKLE_FILE, 'r') as z:
+            with z.open(z.namelist()[0], 'r') as f: # gets the trees.pkl file
+                return pickle.load(f)
 
     # if there's no pre-built tree, build from scratch
     print("no pickle found, building tree from scratch...")
-    df = pd.read_csv(CSV_FILE)
+    df = pd.read_csv(CSV_FILE, compression='zip')
 
     kd_root = None
     quad_root = None
@@ -36,8 +38,14 @@ def build_trees():
         kd_root = insert_rec(kd_root, point, data)
         quad_root = insert_quad(quad_root, point, data)
 
-    # pickle it for next time
-    with open(PICKLE_FILE, "wb") as f:
+    # pickle and zip it for next time
+    unzipped_pickle = "data/trees.pkl"
+    with open(unzipped_pickle, "wb") as f:
         pickle.dump((kd_root, quad_root), f)
+
+    with zipfile.ZipFile(PICKLE_FILE, "w", zipfile.ZIP_DEFLATED) as zipf:
+        zipf.write(unzipped_pickle, arcname="trees.pkl")
+
+    os.remove(unzipped_pickle) # delete the unzipped file after making the zipped
 
     return kd_root, quad_root
